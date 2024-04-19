2d82ad78f0f05fb425c148de05a411386e8226ed
Mr-Apples <104468094+Mr-Apples@users.noreply.github.com>
2024-04-19 03:35:10 +0000

    Add function to clone a repo

--- src/git_helper/mod.rs
+++ src/git_helper/mod.rs
@@ -1,9 +1,10 @@
-use std::*;
 use git2::Repository;
+use std::*;
 
-fn detect_repos_in_dir(repo_parent_dir: &path::Path) -> Option<Vec<Repository>> {
+// Returns a vector that contains all repositories in a given directory
+pub fn detect_repos_in_dir(repo_storage_dir: &path::Path) -> Option<Vec<Repository>> {
     // Get an iterator for the directory in which to detect repositories
-    let repo_dir_iter = match fs::read_dir(repo_parent_dir) {
+    let repo_dir_iter = match fs::read_dir(repo_storage_dir) {
         Ok(read_dir) => read_dir,
         Err(e) => {
             println!("Void-Builder: {}", e.to_string());
@@ -26,13 +27,49 @@ fn detect_repos_in_dir(repo_parent_dir: &path::Path) -> Option<Vec<Repository>> 
                 };
 
                 repositories.push(repo);
-            },
+            }
             Err(e) => {
                 println!("Void-Builder: {}", e.to_string());
                 return None;
             }
         };
-    };
+    }
 
     return Some(repositories);
+}
+
+// Clones a repository and returns the repository object
+pub fn clone_repo(url: &str, dir: &path::Path) -> Option<Repository> {
+    let mut dir_buf: path::PathBuf = dir.to_path_buf();
+    dir_buf = dir_buf.join(path::Path::new(
+        &url.replace("/", "")
+    ));
+
+    return match git2::Repository::clone(url, dir_buf) {
+        Ok(repo) => Some(repo),
+        Err(e) => {
+            println!("Void-Builder: {}", e.message());
+            return None;
+        }
+    };
+}
+
+pub fn update_repo(repo: &Repository) -> Option<()> {
+    match repo.find_remote("origin") {
+        Ok(mut remote) => {
+            match remote.update_tips(None, true, git2::AutotagOption::Auto, None) {
+                Ok(_) => (),
+                Err(e) => {
+                    println!("Void-Builder: {}", e.message());
+                    return None;
+                }
+            };
+
+            return Some(());
+        },
+        Err(e) => {
+            println!("Void-Builder: {}", e.message());
+            return Some(());
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use git_helper::*;
+
 pub mod git_helper;
 
 fn main() {
-    println!("Hello, world!");
+
 }
