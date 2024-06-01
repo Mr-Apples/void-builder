55e81b5f160c644f164111c55136db84293f50a1
MrApples <aidan@hethos.org>
2024-06-01 17:31:59 +1000

    Remove source() from VoidBuilderError's implementation of std::error::Error

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -3,60 +3,54 @@ use std::fmt::{Debug, Display, Formatter};
 
 #[derive(Debug)]
 /// Struct to contain errors encountered by Void Builder
-pub struct VoidBuilderError<T: Debug + 'static> {
-    source: T,
+pub struct VoidBuilderError {
     message: String
 }
 
-impl<T: Debug> VoidBuilderError<T> {
+impl VoidBuilderError {
     /// Creates a new VoidBuilderError from the given error
-    fn new(message: String, source: T) -> VoidBuilderError<T> {
+    fn new(message: String) -> VoidBuilderError {
         VoidBuilderError {
-            source,
             message
         }
     }
 }
 
-impl<T: Debug> Display for VoidBuilderError<T> {
+impl Display for VoidBuilderError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.message)
     }
 }
 
 /// Implement std::error::Error on VoidBuilderError
-impl<T: Debug + error::Error> error::Error for VoidBuilderError<T> {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        return Some(&self.source);
-    }
-}
+impl error::Error for VoidBuilderError {}
 
 /// Implements From<git2::Error> on VoidBuilderError
-impl From<git2::Error> for VoidBuilderError<git2::Error> {
+impl From<git2::Error> for VoidBuilderError {
     /// Creates a VoidBuilderError from a git2::Error
     fn from(value: git2::Error) -> Self {
-        return VoidBuilderError::new(value.message().to_string(), value);
+        return VoidBuilderError::new(value.message().to_string());
     }
 }
 
 /// Implements From<std::io::Error> on VoidBuilderError
-impl From<io::Error> for VoidBuilderError<io::Error> {
+impl From<io::Error> for VoidBuilderError {
     /// Create a VoidBuilderError from a std::io::Error
     fn from(value: io::Error) -> Self {
-        return VoidBuilderError::new(value.to_string(), value);
+        return VoidBuilderError::new(value.to_string());
     }
 }
 
 /// Implements From<url::ParseError> on VoidBuilderError
-impl From<url::ParseError> for VoidBuilderError<url::ParseError> {
+impl From<url::ParseError> for VoidBuilderError {
     /// Create a VoidBuilderError from a url::ParseError
     fn from(value: url::ParseError) -> Self {
-        return VoidBuilderError::new(value.to_string(), value);
+        return VoidBuilderError::new(value.to_string());
     }
 }
 
-impl From<daemonize::Error> for VoidBuilderError<daemonize::Error> {
+impl From<daemonize::Error> for VoidBuilderError {
     fn from(value: daemonize::Error) -> Self {
-        return VoidBuilderError::new(value.to_string(), value)
+        return VoidBuilderError::new(value.to_string())
     }
 }
